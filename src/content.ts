ea609d4989b7039e9cbd97c9a5cf1fb0702a9c67
felix t <41817193+o2bomb@users.noreply.github.com>
2023-02-14 15:18:00 +0800

    Working extension

---
+++ src/content.ts
@@ -0,0 +1,33 @@
+import type { PlasmoCSConfig } from "plasmo"
+
+export const config: PlasmoCSConfig = {
+  matches: [
+    "https://myanimelist.net/anime/*/*",
+    "https://myanimelist.net/manga/*/*"
+  ],
+  css: ["style.css"],
+  run_at: "document_start"
+}
+
+window.addEventListener("load", () => {
+  const segments = window.location.pathname.split("/")
+  if (segments.length < 4) return
+
+  const isAnimePage = /\d/.test(segments[2])
+  if (!isAnimePage) return
+
+  // Hide hover styles if score is N/A
+  const collection = document.getElementsByClassName("score-label score-na")
+  if (collection.length === 0) return
+  const hideStyle = document.createElement("style")
+  hideStyle.innerText = `
+    div.score-label.score-na::after {
+      display: none;
+    }
+
+    div[data-id="info1"]::after {
+      display: none;
+    }
+  `
+  document.body.appendChild(hideStyle)
+})
