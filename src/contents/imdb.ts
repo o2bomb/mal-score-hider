db8e1476862023249a182f0b6f4a9215e2f5b599
felix t <41817193+o2bomb@users.noreply.github.com>
2023-02-15 16:42:41 +0800

    Fix imdb url matching

--- src/contents/imdb.ts
+++ src/contents/imdb.ts
@@ -1,6 +1,10 @@
 import type { PlasmoCSConfig } from "plasmo"
 
 export const config: PlasmoCSConfig = {
-  matches: ["https://www.imdb.com/", "https://www.imdb.com/title/*"],
+  matches: [
+    "https://www.imdb.com/",
+    "https://www.imdb.com/*",
+    "https://www.imdb.com/title/*"
+  ],
   css: ["../style/imdb.css"]
 }
